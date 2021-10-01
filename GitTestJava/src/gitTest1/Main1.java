5d8babe146dacc0feb853cf56d9a4759fd613778
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-09-30 23:39:14 -0500

    3rd commit

--- GitTestJava/src/gitTest1/Main1.java
+++ GitTestJava/src/gitTest1/Main1.java
@@ -3,7 +3,8 @@ package gitTest1;
 public class Main1 {
 	
 	public static void main (String[] args){
-		System.out.println("Joe Biden is a pedophile.");
+		System.out.println("Joe Biden is the president of the US.");
+		System.out.println("Life is a big Gift from God, we just don't understand.");
 	}
 
 }
