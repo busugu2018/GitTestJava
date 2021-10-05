c831a8dec152dd2639f18b83ff1f8c7d4721045d
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2021-10-05 13:50:06 -0500

    Test test

--- GitTestJava/src/gitTest1/Main1.java
+++ GitTestJava/src/gitTest1/Main1.java
@@ -5,6 +5,7 @@ public class Main1 {
 	public static void main (String[] args){
 		System.out.println("Joe Biden is the president of the US.");
 		System.out.println("Life is a big Gift from God, we just don't understand.");
-	}
+		System.out.println("New Entry...");
+	} 
 
 }
